c44afb70212657b5281e8c35e436bdd46aa55706
andrea roveres <andrea.maria.roveres@accenture.com>
2020-03-24 10:42:00 +0100

    setup project

---
+++ SocialDistancing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialDistancing
+//
+//  Created by andrea roveres on 24/03/2020.
+//  Copyright © 2020 Accenture Interactive. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
