1c08196cb4645681e9afe127474c70d25ae3ba5f
andrea roveres <andrea.maria.roveres@accenture.com>
2020-03-30 16:47:40 +0200

    updated project to iOs native app instead of ARKit app

--- SocialDistancing/ViewController.swift
+++ SocialDistancing/ViewController.swift
@@ -2,24 +2,19 @@
 //  ViewController.swift
 //  SocialDistancing
 //
-//  Created by andrea roveres on 24/03/2020.
-//  Copyright © 2020 Accenture Interactive. All rights reserved.
+//  Created by andrea roveres on 30/03/2020.
+//  Copyright © 2020 AndreaRov. All rights reserved.
 //
 
 import UIKit
-import RealityKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet var arView: ARView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        // Do any additional setup after loading the view.
     }
+
+
 }
+
